511ae9fd25415cd3a34fbc732552dda7a7d6a2b3
Rick B. <richardb.barton@eastman.com>
2025-08-10 22:33:33 -0500

    more tests

--- packages/paper2/tests/Curve.test.ts
+++ packages/paper2/tests/Curve.test.ts
@@ -1,11 +1,28 @@
 import { describe, expect, it } from 'vitest';
-import { ref } from '~/globals';
 import { Point } from '~/basic/Point';
 import { Curve } from '~/path/Curve';
 import { Path } from '~/path/Path';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function testClassify(curve, expeced, message = '') {
+// https://github.com/paperjs/paper.js/blob/develop/test/helpers.js
+// Point: function(actual, expected, message, options) {
+//     comparators.Number(actual.x, expected.x, message + ' (#x)', options);
+//     comparators.Number(actual.y, expected.y, message + ' (#y)', options);
+// },
+// Number: function(actual, expected, message, options) {
+//     // Compare with a default tolerance of 1e-5:
+//     var ok = Math.abs(actual - expected)
+//             <= Base.pick(options && options.tolerance, 1e-5);
+//     QUnit.push(ok, ok ? expected : actual, expected, message);
+// },
+
+function pointCompare(actual, expected) {
+  // Compare with a default tolerance of 1e-5:
+  const okX = Math.abs(actual.x - expected.x) <= 1e-5;
+  const okY = Math.abs(actual.y - expected.y) <= 1e-5;
+  return okX && okY;
+}
+
+function testClassify(curve, expeced) {
   const info = curve.classify();
   if (expeced.type) {
     // equals(info.type, expeced.type, "info.type == '" + expeced.type + "'");
@@ -25,7 +42,7 @@ function testClassify(curve, expeced, message = '') {
 describe('Given: Curve Class', () => {
   describe('WHEN Curve#classify()', () => {
     // eslint-disable-next-line vitest/expect-expect
-    it('THEN should todo', () => {
+    it('THEN should return expected numbers', () => {
       const point = new Curve([100, 100], null, null, [100, 100]);
       const line = new Curve([100, 100], null, null, [200, 200]);
       const cusp = new Curve([100, 200], [100, -100], [-100, -100], [200, 200]);
@@ -51,9 +68,7 @@ describe('Given: Curve Class', () => {
   });
 
   describe('WHEN Curve#getPointAtTime()', () => {
-    it('THEN should todo', () => {
-      ref.paper.setup(new ref.paper.Size(100, 100));
-
+    it('THEN should return expected numbers', () => {
       let curve = new Path.Circle({
         center: [100, 100],
         radius: 100,
@@ -68,21 +83,12 @@ describe('Given: Curve Class', () => {
       ];
 
       for (let i = 0; i < points.length; i++) {
-        // TODO: the precision is much more than expected:
-        // - Expected
-        // + Received
-        // Point2 {
-        // -   "x": 7.8585,
-        // -   "y": 61.07549,
-        // +   "x": 7.858495705504465,
-        // +   "y": 61.07548711651339,
-        // }
-        // const entry = points[i];
+        const entry = points[i];
         // equals(curve.getPointAtTime(entry[0]), entry[1], 'curve.getPointAtTime(' + entry[0] + ');');
-        // expect(curve.getPointAtTime(entry[0])).toEqual(entry[1]);
+        expect(pointCompare(curve.getPointAtTime(entry[0]), entry[1])).toBe(true);
         // // Legacy version:
         // equals(curve.getPointAt(entry[0], true), entry[1], 'Legacy: curve.getPointAt(' + entry[0] + ', true);');
-        // expect(curve.getPointAt(entry[0], true)).toEqual(entry[1]);
+        expect(pointCompare(curve.getPointAt(entry[0], true), entry[1])).toBe(true);
       }
 
       // equals(curve.getPointAt(curve.length + 1), null, 'Should return null when offset is out of range.');
@@ -97,6 +103,48 @@ describe('Given: Curve Class', () => {
       expect(curve.getPointAtTime(0).y).toBe(curve.point1.y);
       // equals(curve.getPointAtTime(1).y, curve.point2.y, 'Point at t=1 should not deviate from the actual coordinates.');
       expect(curve.getPointAtTime(1).y).toBe(curve.point2.y);
+    });
+  });
+
+  describe('WHEN Curve#getTangentAtTime()', () => {
+    it('THEN should return expected numbers', () => {
+      const curve = new Path.Circle({
+        center: [100, 100],
+        radius: 100,
+      }).firstCurve;
+
+      const tangents = [
+        [0, new Point(0, -165.68542)],
+        [0.25, new Point(60.7233, -143.56602)],
+        [0.5, new Point(108.57864, -108.57864)],
+        [0.75, new Point(143.56602, -60.7233)],
+        [1, new Point(165.68542, 0)],
+      ];
+
+      for (let i = 0; i < tangents.length; i++) {
+        const entry = tangents[i];
+
+        // equals(curve.getTangentAtTime(entry[0]), entry[1].normalize(), 'curve.getTangentAtTime(' + entry[0] + ');');
+        expect(pointCompare(curve.getTangentAtTime(entry[0]), entry[1].normalize())).toBe(true);
+
+        // equals(curve.getWeightedTangentAtTime(entry[0]), entry[1], 'curve.getWeightedTangentAtTime(' + entry[0] + ');');
+        expect(pointCompare(curve.getWeightedTangentAtTime(entry[0]), entry[1])).toBe(true);
+
+        // // Legacy version:
+        // equals(
+        //   curve.getTangentAt(entry[0], true),
+        //   entry[1].normalize(),
+        //   'Legacy: curve.getTangentAt(' + entry[0] + ', true);'
+        // );
+        expect(pointCompare(curve.getTangentAt(entry[0], true), entry[1].normalize())).toBe(true);
+
+        // equals(
+        //   curve.getWeightedTangentAt(entry[0], true),
+        //   entry[1],
+        //   'Legacy: curve.getWeightedTangentAt(' + entry[0] + ', true);'
+        // );
+        expect(pointCompare(curve.getWeightedTangentAt(entry[0], true), entry[1])).toBe(true);
+      }
     });
   });
 });
--- packages/paper2/tests/setup.ts
+++ packages/paper2/tests/setup.ts
@@ -1,2 +1,8 @@
+import { beforeEach } from 'vitest';
 import './mocks/canvas';
 import '~/index';
+import { ref } from '~/globals';
+
+beforeEach(() => {
+  ref.paper.setup(new ref.paper.Size(100, 100));
+});
