8a30de4b612693df99c3e0f53304981282129d97
Rick B. <richardb.barton@eastman.com>
2025-08-19 23:14:52 -0500

    fix docs

--- docs/paper2-stackblitz.js
+++ docs/paper2-stackblitz.js
@@ -1,17 +1,26 @@
 function openInStackblitz() {
+  const type = document.querySelector("paper2-stackblitz").getAttribute("type");
   const paper2Sb = document.querySelector("paper2-stackblitz").shadowRoot;
 
   let html = `
         <link rel="stylesheet" href="https://emn-dev.github.io/paper2/paper2-docs.css" />
             <canvas id='myCanvas' resize></canvas>
-            <script canvas="myCanvas" type="text/paperscript">`;
+            `;
+  if (type === "core") {
+    html += `<script>`;
+  } else {
+    html += `<script canvas="myCanvas" type="text/paperscript">`;
+  }
   const mainPaper2Script1 = document.getElementById("mainPaper2Script1");
   html += mainPaper2Script1.innerHTML + `<\/script>`;
-  const mainPaper2Script2 = document.getElementById("mainPaper2Script2");
-  if (mainPaper2Script2) {
-    html += `
+
+  if (type !== "core") {
+    const mainPaper2Script2 = document.getElementById("mainPaper2Script2");
+    if (mainPaper2Script2) {
+      html += `
         <script>`;
-    html += mainPaper2Script2.innerHTML + `<\/script>`;
+      html += mainPaper2Script2.innerHTML + `<\/script>`;
+    }
   }
 
   const sbInputHtml = paper2Sb.getElementById("sbInputHtml");
