89acce34b7d305d973c2077bb59c934a527879b3
Rick B. <richardb.barton@eastman.com>
2025-08-29 11:23:26 -0500

    try out cy tests

---
+++ cypress/e2e/json/circleTesting.cy.ts
@@ -0,0 +1,15 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+describe(`GIVEN: Circle Testing (JSON)`, () => {
+  beforeEach(() => {
+    cy.visit(`/json-circleTesting.html`);
+    cy.wait(750); // Allow timeout to compelete
+  });
+
+  describe("WHEN page has loaded", () => {
+    it("THEN should pass visual image compare", () => {
+      const snapPath = "json/circleTesting";
+
+      cy.get("main").matchImageSnapshot(snapPath);
+    });
+  });
+});
---
+++ cypress/types-other/index.ts
@@ -0,0 +1,10 @@
+export {};
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      matchImageSnapshot(name?: string, options?: object): void;
+    }
+  }
+}
