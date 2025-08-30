e47cab57e3d116e69c748ffc968670b648462886
Rick B. <richardb.barton@eastman.com>
2025-08-29 19:56:41 -0500

    new cy test

---
+++ cypress/e2e/svg-import-inkscape.cy.ts
@@ -0,0 +1,15 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+describe(`GIVEN: Inkscape`, () => {
+  beforeEach(() => {
+    cy.visit(`/svg-import-inkscape.html`);
+    cy.wait(999); // Allow timeout to compelete
+  });
+
+  describe("WHEN page has loaded", () => {
+    it("THEN should pass visual image compare", () => {
+      const snapPath = "svg-import/inkscape";
+
+      cy.get("main").matchImageSnapshot(snapPath);
+    });
+  });
+});
