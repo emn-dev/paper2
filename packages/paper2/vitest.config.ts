c2c88b75b1c58b6da3f8838b8566cfd605746089
Rick B. <richardb.barton@eastman.com>
2025-09-02 15:26:27 -0500

    coverage cmd

--- packages/paper2/vitest.config.ts
+++ packages/paper2/vitest.config.ts
@@ -3,15 +3,10 @@ import path from 'path';
 import { configDefaults, defineConfig } from 'vitest/config';
 // import viteConfig from './vite.config';
 
-console.log('a=sd-fas=-fd=a-sdf=-as=f-as=-fd-a=s-fd-sa=df-=asd=f=sadf');
-console.log('a=sd-fas=-fd=a-sdf=-as=f-as=-fd-a=s-fd-sa=df-=asd=f=sadf');
-console.log(process.env);
-
 export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      enabled: true, // Comment this out to stop running the report when working locally
       include: ['lib/*'],
       reportsDirectory: '../../docs/test_coverage',
     },
