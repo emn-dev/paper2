4cebedccff1bedc47ee513e8dec8f4170ae1bde7
Rick B. <richardb.barton@eastman.com>
2025-09-02 15:17:03 -0500

    fixes

--- packages/paper2/tests/TextItem.test.ts
+++ packages/paper2/tests/TextItem.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from 'vitest';
-import { Color, Point, PointText } from '~/index-core';
+import { Color, Point, PointText, Rectangle } from '~/index-core';
 import { equals } from './_helpers';
 
 describe('Given: TextItem', () => {
@@ -12,8 +12,7 @@ describe('Given: TextItem', () => {
     });
     equals(text.fillColor, new Color(0, 0, 0), 'text.fillColor should be black by default');
     equals(text.point, new Point(100, 100), 'text.point');
-    // TODO: why is this broken?
-    // equals(text.bounds, new Rectangle(100, 87.4, 76.25, 16.8), 'text.bounds', { tolerance: 1.0 });
+    equals(text.bounds, new Rectangle(100, 87.4, 76.25, 16.8), 'text.bounds', { tolerance: 1.0 });
     equals(function () {
       return text.hitTest(text.bounds.center) != null;
     }, true);
