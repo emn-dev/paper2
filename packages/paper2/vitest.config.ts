b016a5cef99c9eb39e9fb466009c5b88217f4800
Rick B. <richardb.barton@eastman.com>
2025-09-02 15:18:45 -0500

    checking something

--- packages/paper2/vitest.config.ts
+++ packages/paper2/vitest.config.ts
@@ -3,6 +3,10 @@ import path from 'path';
 import { configDefaults, defineConfig } from 'vitest/config';
 // import viteConfig from './vite.config';
 
+console.log('a=sd-fas=-fd=a-sdf=-as=f-as=-fd-a=s-fd-sa=df-=asd=f=sadf');
+console.log('a=sd-fas=-fd=a-sdf=-as=f-as=-fd-a=s-fd-sa=df-=asd=f=sadf');
+console.log(process.env);
+
 export default defineConfig({
   test: {
     coverage: {
