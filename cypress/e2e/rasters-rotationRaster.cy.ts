735d51548d66db5629558c533acb14f4b008a8e2
Rick B. <richardb.barton@eastman.com>
2025-09-03 13:26:58 -0500

    examples and tests

---
+++ cypress/e2e/rasters-rotationRaster.cy.ts
@@ -0,0 +1,16 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+describe(`GIVEN: Rotation Raster`, () => {
+  const type = "rasters";
+  const name = "rotationRaster";
+
+  beforeEach(() => {
+    cy.visit(`/${type}-${name}.html`);
+    cy.wait(500); // Allow some time for loading
+  });
+
+  describe("WHEN page has loaded", () => {
+    it("THEN should pass visual image compare", () => {
+      cy.get("canvas").matchImageSnapshot(`${type}/${name}`);
+    });
+  });
+});
---
+++ cypress/e2e/rasters-smoothing.cy.ts
@@ -0,0 +1,16 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+describe(`GIVEN: Smoothing`, () => {
+  const type = "rasters";
+  const name = "smoothing";
+
+  beforeEach(() => {
+    cy.visit(`/${type}-${name}.html`);
+    cy.wait(500); // Allow some time for loading
+  });
+
+  describe("WHEN page has loaded", () => {
+    it("THEN should pass visual image compare", () => {
+      cy.get("canvas").matchImageSnapshot(`${type}/${name}`);
+    });
+  });
+});
