f0ae1b7e49be6c0fa8c3e0c34cdb3df712522fc4
Rick B. <richardb.barton@eastman.com>
2025-09-03 08:08:03 -0500

    remove nav menu icon

--- cypress/e2e/scripts-strokeScaling.cy.ts
+++ cypress/e2e/scripts-strokeScaling.cy.ts
@@ -6,13 +6,16 @@ describe(`GIVEN: Shapes 2`, () => {
   beforeEach(() => {
     cy.visit(`/${type}-${name}.html`);
     cy.wait(500); // Allow some time for loading
+    cy.get("paper2-nav").invoke("remove");
   });
 
   describe("WHEN page has loaded", () => {
     it("THEN should pass visual image compare", () => {
-      cy.get("main").matchImageSnapshot(`${type}/${name}`, {
-        failureThreshold: 200, // If less than 200 pixels changed it is still a success
-      });
+      cy.get("main").matchImageSnapshot(`${type}/${name}`);
+
+      // cy.get("main").matchImageSnapshot(`${type}/${name}`, {
+      //   failureThreshold: 200, // If less than 200 pixels changed it is still a success
+      // });
     });
   });
 });
