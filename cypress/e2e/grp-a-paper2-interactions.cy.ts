d7dcc067bc63434c5782af9f76b02ef75c98b1f0
Rick B. <richardb.barton@eastman.com>
2025-09-08 22:59:30 -0500

    move Interaction tests to Cypress

--- cypress/e2e/grp-a-paper2-interactions.cy.ts
+++ cypress/e2e/grp-a-paper2-interactions.cy.ts
@@ -26,10 +26,12 @@ describe(`GIVEN: Interactions`, () => {
     });
 
     it("Item#onMouseDown() with stroked item", () => {
-      cy.visit(`/${type}-${name}.html?canvasId=test2`);
+      const testId = "test2";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
       cy.wait(99);
 
-      cy.get("#test2").trigger("mousedown", 10, 10);
+      cy.get(`#${testId}`).trigger("mousedown", 10, 10);
 
       cy.window().then((win: any) => {
         const test = win.interactions;
@@ -43,45 +45,53 @@ describe(`GIVEN: Interactions`, () => {
     });
 
     it("Item#onMouseDown() is not triggered when item is not filled", () => {
-      cy.visit(`/${type}-${name}.html?canvasId=test3`);
+      const testId = "test3";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
       cy.wait(99);
 
-      cy.get("#test3").trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
 
-      cy.get("#test3").should("have.a.property", "resize");
+      cy.get(`#${testId}`).should("have.a.property", "resize");
 
       // This test succeeds if no error is thrown
     });
 
     it("Item#onMouseDown() is not triggered when item is not visible", () => {
-      cy.visit(`/${type}-${name}.html?canvasId=test4`);
+      const testId = "test4";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
       cy.wait(99);
 
-      cy.get("#test4").trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
 
-      cy.get("#test4").should("have.a.property", "resize");
+      cy.get(`#${testId}`).should("have.a.property", "resize");
 
       // This test succeeds if no error is thrown
     });
 
     it("Item#onMouseDown() is not triggered when item is locked", () => {
-      cy.visit(`/${type}-${name}.html?canvasId=test5`);
+      const testId = "test5";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
       cy.wait(99);
 
-      cy.get("#test5").trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
 
-      cy.get("#test5").should("have.a.property", "resize");
+      cy.get(`#${testId}`).should("have.a.property", "resize");
 
       // This test succeeds if no error is thrown
     });
 
     it("Item#onMouseDown() is not triggered when another item is in front", () => {
-      cy.visit(`/${type}-${name}.html?canvasId=test6`);
+      const testId = "test6";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
       cy.wait(99);
 
-      cy.get("#test6").trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
 
-      cy.get("#test6").should("have.a.property", "resize");
+      cy.get(`#${testId}`).should("have.a.property", "resize");
 
       // This test succeeds if no error is thrown
     });
@@ -172,7 +182,8 @@ describe(`GIVEN: Interactions`, () => {
       cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
       cy.wait(99);
 
-      cy.get(`#${testId}`).click(20, 20);
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 20);
 
       cy.window().then((win: any) => {
         const test = win.interactions;
@@ -183,6 +194,204 @@ describe(`GIVEN: Interactions`, () => {
         equals(test.currentTarget.actual, test.currentTarget.expected);
         equals(test.delta.actual, test.delta.expected);
       });
+    });
+
+    it("Item#onClick() is not triggered if up point is not on item", () => {
+      const testId = "test13";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 75);
+
+      cy.get(`#${testId}`).should("have.a.property", "resize");
+
+      // This test succeeds if no error is thrown
+    });
+
+    it("Item#onClick() is not triggered if down point is not on item", () => {
+      const testId = "test14";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 20, 75);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 20);
+
+      cy.get(`#${testId}`).should("have.a.property", "resize");
+
+      // This test succeeds if no error is thrown
+    });
+
+    it("Item#onDoubleClick()", () => {
+      const testId = "test15";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 20);
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 20);
+
+      cy.window().then((win: any) => {
+        const test = win.interactions;
+
+        equals(test.type.actual, test.type.expected);
+        equals(test.point.actual, test.point.expected);
+        equals(test.target.actual, test.target.expected);
+        equals(test.currentTarget.actual, test.currentTarget.expected);
+        equals(test.delta.actual, test.delta.expected);
+      });
+    });
+
+    it("Item#onDoubleClick() is not triggered if both clicks are not on same item", () => {
+      const testId = "test16";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 20);
+      cy.get(`#${testId}`).trigger("mousedown", 35, 35);
+      cy.get(`#${testId}`).trigger("mouseup", 35, 35);
+
+      cy.get(`#${testId}`).should("have.a.property", "resize");
+
+      // This test succeeds if no error is thrown
+    });
+
+    it("Item#onDoubleClick() is not triggered if time between both clicks is too long", () => {
+      const testId = "test17";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 20);
+      cy.wait(301);
+      cy.get(`#${testId}`).trigger("mousedown", 20, 20);
+      cy.get(`#${testId}`).trigger("mouseup", 20, 20);
+
+      cy.get(`#${testId}`).should("have.a.property", "resize");
+
+      // This test succeeds if no error is thrown
+    });
+
+    it("Item#onMouseEnter()", () => {
+      const testId = "test18";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousemove", 20, 20);
+
+      cy.window().then((win: any) => {
+        const test = win.interactions;
+
+        equals(test.type.actual, test.type.expected);
+        equals(test.point.actual, test.point.expected);
+        equals(test.target.actual, test.target.expected);
+        equals(test.currentTarget.actual, test.currentTarget.expected);
+        equals(test.delta.actual, test.delta.expected);
+      });
+    });
+
+    it("Item#onMouseEnter() is only re-triggered after mouse leave", () => {
+      const testId = "test19";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      // enter
+      cy.get(`#${testId}`).trigger("mousemove", 8, 8);
+      cy.get(`#${testId}`).trigger("mousemove", 12, 12);
+      cy.get(`#${testId}`).trigger("mousemove", 16, 16);
+
+      // leave
+      cy.get(`#${testId}`).trigger("mousemove", 8, 8);
+
+      // re-enter
+      cy.get(`#${testId}`).trigger("mousemove", 12, 12);
+
+      cy.window().then((win: any) => {
+        const test = win.interactions;
+
+        equals(test, 2);
+      });
+    });
+
+    it("Item#onMouseLeave()", () => {
+      const testId = "test20";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousemove", 12, 12);
+      cy.get(`#${testId}`).trigger("mousemove", 8, 8);
+
+      cy.window().then((win: any) => {
+        const test = win.interactions;
+
+        equals(test.type.actual, test.type.expected);
+        equals(test.point.actual, test.point.expected);
+        equals(test.target.actual, test.target.expected);
+        equals(test.currentTarget.actual, test.currentTarget.expected);
+        equals(test.delta.actual, test.delta.expected);
+      });
+    });
+
+    it("Item#onMouseDrag()", () => {
+      const testId = "test21";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 12, 12);
+      cy.get(`#${testId}`).trigger("mousemove", 8, 8);
+
+      cy.window().then((win: any) => {
+        const test = win.interactions;
+
+        equals(test.type.actual, test.type.expected);
+        equals(test.point.actual, test.point.expected);
+        equals(test.target.actual, test.target.expected);
+        equals(test.currentTarget.actual, test.currentTarget.expected);
+        equals(test.delta.actual, test.delta.expected);
+      });
+    });
+
+    it("Item#onMouseDrag() is not triggered after mouse up", () => {
+      const testId = "test22";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 12, 12);
+      cy.get(`#${testId}`).trigger("mousemove", 14, 14);
+      cy.get(`#${testId}`).trigger("mouseup", 14, 14);
+      cy.get(`#${testId}`).trigger("mousemove", 16, 16);
+
+      cy.window().then((win: any) => {
+        const test = win.interactions;
+
+        equals(test, 1);
+      });
+    });
+
+    it("Item#onMouseDrag() is not triggered if mouse down was on another item", () => {
+      const testId = "test23";
+
+      cy.visit(`/${type}-${name}.html?canvasId=${testId}`);
+      cy.wait(99);
+
+      cy.get(`#${testId}`).trigger("mousedown", 35, 35);
+      cy.get(`#${testId}`).trigger("mousemove", 45, 45);
+
+      cy.get(`#${testId}`).should("have.a.property", "resize");
+
+      // This test succeeds if no error is thrown
     });
   });
 });
